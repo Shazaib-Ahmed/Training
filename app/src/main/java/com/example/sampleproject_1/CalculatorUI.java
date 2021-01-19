7f94447ddd791461bc557036b988a8937585a1c5
Shazaib-Ahmed <shazzy1901.sa@gmail.com>
2021-01-19 09:36:05 +0530

    First Commit

--- app/src/main/java/com/example/sampleproject_1/CalculatorUI.java
+++ app/src/main/java/com/example/sampleproject_1/CalculatorUI.java
@@ -10,5 +10,6 @@ public class CalculatorUI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator_u_i);
+        setTitle("Calculator");
     }
 }
