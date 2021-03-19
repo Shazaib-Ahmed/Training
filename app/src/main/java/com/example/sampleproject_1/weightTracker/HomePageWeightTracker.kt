7ce67f6808aec5f2f769c911476294fc700d648f
shazaib-ahmed <shazzy1901.sa@gmail.com>
2021-03-19 19:28:30 +0530

    first commit

--- app/src/main/java/com/example/sampleproject_1/weightTracker/HomePageWeightTracker.kt
+++ app/src/main/java/com/example/sampleproject_1/weightTracker/HomePageWeightTracker.kt
@@ -3,6 +3,8 @@ package com.example.sampleproject_1.weightTracker
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import com.example.sampleproject_1.R
 import com.example.sampleproject_1.WaterReminder.Utils.AppUtils
@@ -13,6 +15,8 @@ private lateinit var goalWeight: TextView
 private var startingWeightKey = 0
 private var goalWeightKey = 0
 
+private lateinit var spinnerWeightTracker: Spinner
+
 private lateinit var sharedPreferences: SharedPreferences
 
 class HomePageWeightTracker : AppCompatActivity() {
@@ -20,6 +24,7 @@ class HomePageWeightTracker : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page_weight_tracker)
         initialisationFields()
+        setUpSpinner()
 
         startingWeightKey = sharedPreferences.getInt(AppUtils.INITIAL_WEIGHT_KEY_WT.toString(), 0)
 
@@ -34,6 +39,13 @@ class HomePageWeightTracker : AppCompatActivity() {
         sharedPreferences = this.getSharedPreferences(AppUtils.USERS_SHARED_PREF, AppUtils.PRIVATE_MODE)
         startingWeight = findViewById(R.id.starting_weightTV)
         goalWeight = findViewById(R.id.goal_weightTV)
+        spinnerWeightTracker = findViewById(R.id.spinner_wt)
+    }
+
+    private fun setUpSpinner() {
+        val sortBySpinnerData = resources.getStringArray(R.array.sortByspinner)
+        val ad = ArrayAdapter(this.applicationContext, android.R.layout.simple_dropdown_item_1line, sortBySpinnerData)
+        spinnerWeightTracker!!.adapter = ad
     }
 
 }
